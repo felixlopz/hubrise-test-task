45b01febedbce8cd8ea9a222e74ead89ca42f495
antmonnier <monniera@gmail.com>
2021-01-11 20:14:10 +0100

    New footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,75 +1,88 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-
-import Link from './link'
-
-import { generateKey, getLanguageFromAbsolutePath } from './utils'
-
-import logo from '../images/logo_footer.png'
-import hero from '../images/hero_image_optimized.jpg'
 import { graphql, useStaticQuery } from 'gatsby'
+import Link from './link'
+import { generateKey, getLanguageFromAbsolutePath } from './utils'
+import logo from '../images/logo_footer.png'
 
 const Footer = ({ pageContext }) => {
-  const { t } = useTranslation()
-
   const menuFooterNodes = useStaticQuery(footerQuery).allFile.nodes
-  const menuItems = menuFooterNodes.find(
+  const menuSections = menuFooterNodes.find(
     ({ absolutePath }) =>
       getLanguageFromAbsolutePath(absolutePath) === pageContext.lang
-  ).childYaml.parsedContent
+  ).childYaml.parsedContent.sections
 
   return (
-    <footer
-      className="footer"
-      style={{
-        backgroundImage: `url(${hero})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover'
-      }}
-      data-floater-footer
-    >
-      <div className="footer__in">
-        <div className="footer__block">
-          <ul className="footer-menu">
-            {menuItems.map(({ title, to }, idx) => (
-              <li key={generateKey(title, idx)} className="footer-menu__item">
-                <Link to={to} className="footer-menu__link">
-                  {title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className="footer__logo">
-            <img src={logo} alt="company-logo" />
-          </div>
+    <footer className="footer">
+      <div className="footer__nav-wrapper">
+        <div className="footer__nav footer-nav">
+          {menuSections.map((section, idx) => (
+            <FooterSection {...section} />
+          ))}
         </div>
-        <div className="footer__contacts">
-          <h5 className="footer__title">{t(`layout.footer.contacts.title`)}</h5>
-          <Link
-            className="footer__contact-mail"
-            to="mailto:contact@hubrise.com"
-          >
-            {t(`layout.footer.contacts.email`)}
-          </Link>
-        </div>
-        <button
-          className="footer__scroll-up"
-          id="scroll-top"
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-        >
-          <i className="fa fa-angle-up" />
-        </button>
+        <ScrollUpButton />
       </div>
-      <div className="footer__copyright">
-        <span className="footer__copyright-span">
-          {t(`layout.footer.copyright`, {
-            year: new Date(Date.now()).getFullYear()
-          })}
-        </span>
-      </div>
+      <Copyright />
     </footer>
   )
 }
+
+const FooterSection = ({ title, links }) => (
+  <div className="footer-nav__section">
+    <div className="footer-nav__header">{title}</div>
+    <ul className="footer-nav__list">
+      {links.map(({ title, to }, idx) => (
+        <li key={generateKey(title, idx)} className="footer-nav__item">
+          <Link to={to} className="footer-nav__item-link">
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+const Copyright = () => (
+  <div className="footer__copyright-wrapper">
+    <div className="footer__copyright footer-copyright">
+      <div className="footer-copyright__company">
+        &copy; {new Date(Date.now()).getFullYear()} HubRise
+      </div>
+      <div className="footer-copyright__logo">
+        <img src={logo} alt="HubRise" />
+      </div>
+      <div className="footer-copyright__contact">
+        <Link
+          className="footer-copyright__contact__email"
+          to="mailto:contact@hubrise.com"
+        >
+          contact@hubrise.com
+        </Link>
+        <Link
+          className="footer-copyright__contact__linkedin"
+          to="https://www.linkedin.com/company/hubrise"
+        >
+          <i className="fa fa-linkedin-square" />
+        </Link>
+        <Link
+          className="footer-copyright__contact__twitter"
+          to="https://twitter.com/HubRiseHQ"
+        >
+          <i className="fa fa-twitter-square" />
+        </Link>
+      </div>
+    </div>
+  </div>
+)
+
+const ScrollUpButton = () => (
+  <button
+    className="footer__scroll-up"
+    id="scroll-top"
+    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+  >
+    <i className="fa fa-angle-up" />
+  </button>
+)
 
 const footerQuery = graphql`
   query getFooterData {
