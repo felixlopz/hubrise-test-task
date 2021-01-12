4cccda024360c92dd454a5f201ef6ca99d2000d7
antmonnier <monniera@gmail.com>
2021-01-12 15:57:08 +0100

    Frontpage hero new design (not completed yet)

--- src/components/pages/frontpage/hero.jsx
+++ src/components/pages/frontpage/hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from '../../link'
 
 import { markdownToHtml } from '../../utils'
 
@@ -7,15 +8,18 @@ export const Hero = ({ title, description, button_label, button_url }) => {
   return (
     <div className="frontpage-hero">
       <div className="frontpage-hero__container">
-        <div className="frontpage-hero__banner">
-          <div className="frontpage-hero__banner-in">
-            <h3 className="frontpage-hero__title">{title}</h3>
+        <div className="frontpage-hero__banner-wrapper">
+          <div className="frontpage-hero__banner">
+            <h1 className="frontpage-hero__title">{title}</h1>
             <div
               className="frontpage-hero__description"
               dangerouslySetInnerHTML={{
                 __html: markdownToHtml(description)
               }}
             ></div>
+            <Link className="frontpage-hero__button" to={button_url}>
+              {button_label}
+            </Link>
           </div>
         </div>
       </div>
@@ -25,5 +29,7 @@ export const Hero = ({ title, description, button_label, button_url }) => {
 
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
+  button_label: PropTypes.string.isRequired,
+  button_url: PropTypes.string.isRequired
 }
