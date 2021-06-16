d640ddbc06b5416d5d6750fb6ceecd34c04bb61f
antmonnier <monniera@gmail.com>
2021-06-16 23:37:41 +0200

    Fixed language switcher for the pricing page

--- src/gatsby-apis/node-api/util/createYamlPages.ts
+++ src/gatsby-apis/node-api/util/createYamlPages.ts
@@ -1,19 +1,23 @@
 import { CreatePagesArgs } from 'gatsby'
 
+import { YamlContext } from 'utils/context'
+import { LocaleCode, localeCodes } from 'utils/locales'
+
 import { generateLanguagePaths, parseRelativePath } from './locale'
 import { pathWithLocale } from './urls'
 import { getLayoutPath } from './layout'
-import { YamlContext } from '../../../utils/context'
 
-export interface YamlData {
+interface YamlData {
   allFile: {
-    nodes: Array<{
-      absolutePath
-      id
-      childYaml: {
-        parsedContent: any
-      }
-    }>
+    nodes: Array<YamlNode>
+  }
+}
+
+interface YamlNode {
+  absolutePath
+  id
+  childYaml: {
+    parsedContent: any
   }
 }
 
@@ -41,13 +45,26 @@ export async function createYamlPages(
   if (errors) throw errors
   if (!data) throw 'GraphQL returned no data'
 
-  data.allFile.nodes.forEach((node) => {
-    const localeCode = parseRelativePath(node.absolutePath).localeCode
-    const getPath = (localeCode) =>
-      pathWithLocale(localeCode, node.childYaml.parsedContent.path)
+  const getNode = (localeCode: LocaleCode): YamlNode => {
+    const node = data.allFile.nodes.find(
+      (node) => parseRelativePath(node.absolutePath).localeCode === localeCode
+    )
+    if (!node) throw `No ${yamlFile} file found for locale ${localeCode}`
+    return node
+  }
+
+  const getPath = (localeCode: LocaleCode): string => {
+    return pathWithLocale(
+      localeCode,
+      getNode(localeCode).childYaml.parsedContent.path
+    )
+  }
+
+  localeCodes.forEach((localeCode) => {
+    const node = getNode(localeCode)
 
     actions.createPage<YamlContext>({
-      path: getPath(localeCode),
+      path: pathWithLocale(localeCode, node.childYaml.parsedContent.path),
       component: getLayoutPath(layoutName),
       context: {
         id: node.id,
