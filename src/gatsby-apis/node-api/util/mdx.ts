d560ae2e6a4f47651b197bf4d971cae421c26f2f
antmonnier <monniera@gmail.com>
2023-05-15 20:54:39 +0200

    Fix build target

--- src/gatsby-apis/node-api/util/mdx.ts
+++ src/gatsby-apis/node-api/util/mdx.ts
@@ -5,5 +5,5 @@ export type MdxNodeType = "documentation" | "blog"
  * @param fileAbsolutePath The absolute path of the node's file.
  */
 export const mdxNodeType = (fileAbsolutePath: string): MdxNodeType => {
-  return fileAbsolutePath.includes("/var/www/website/content/blog/") ? "blog" : "documentation"
+  return fileAbsolutePath.includes("/website/content/blog/") ? "blog" : "documentation"
 }
