6b8cefb99b44dda9f34b22aede32b13cc174e67c
antmonnier <monniera@gmail.com>
2023-09-07 20:18:51 +0200

    Fix Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress"
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    defaultCommandTimeout: +process.env.CYPRESS_DEFAULT_COMMAND_TIMEOUT || 4000,
     viewportWidth: 1280,
     viewportHeight: 720,
     setupNodeEvents(_on, _config) {
