cd15939a9375bc0df5303028759bd0c9db14ea3a
antmonnier <monniera@gmail.com>
2023-09-08 08:27:55 +0200

    Cypress test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,57 +1,75 @@
 /// <reference types="cypress" />
 
-// DEBUGGING
+// To help debugging in GitHub Actions
 beforeEach(() => {
   cy.on("window:console", (msg) => {
     console.log(msg)
   })
 })
 
-describe("doesn't crash and burn", () => {
-  const pages = [
-    "/",
-    "/apps",
-    "/pricing",
-    "/apps/livepepper",
-    "/apps/livepepper/connect-hubrise",
-    "/developers",
-    "/developers/api/account-management",
-    "/fr",
-    "/fr/apps",
-    "/fr/developers/api/account-management",
-    "/blog",
-    "/blog/catalog-variants",
-  ]
-
-  pages.forEach((page) => {
+describe("Pages", () => {
+  ;[
+    ["/", "Integration for retail"],
+    ["/apps", "Integrated Apps"],
+    ["/pricing", "Fair Pricing"],
+    ["/apps/livepepper", "Overview"],
+    ["/apps/livepepper/connect-hubrise", "Connect to HubRise"],
+    ["/developers", "Quick Start"],
+    ["/developers/api/account-management", "Account Management"],
+    ["/fr", "Intégration pour le commerce"],
+    ["/fr/apps", "Applications intégrées"],
+    ["/fr/developers/api/account-management", "Account Management"],
+    ["/blog", "The HubRise Blog"],
+    ["/blog/catalog-variants", "Catalog Variants"],
+  ].forEach(([page, content]) => {
     it(`renders ${page}`, () => {
       cy.visit(page)
-      cy.contains("Apps")
+      cy.contains(content)
     })
+  })
+
+  it("renders the homepage", () => {
+    cy.visit("/")
+    cy.contains("Integration for retail")
+  })
+
+  it("renders apps on the Apps page", () => {
+    cy.visit("/fr")
+    cy.contains("li", "Apps").click()
+    cy.contains("LivePepper")
+  })
+
+  it("shows a warning on non translated pages", () => {
+    cy.visit("/fr/developers/api/account-management")
+    cy.contains("Cette documentation est disponible en anglais uniquement.")
   })
 })
 
-it("opens the 'Contact us' modal and closes it", () => {
-  const modal = () => cy.get("[role='modal']")
-
-  cy.visit("/")
-  cy.contains("Integration for retail")
-  cy.contains("li", "Developers").should("be.visible").click()
-  cy.contains("Contact us").should("be.visible").click()
-  modal().should("be.visible")
-
-  modal().find("[data-testid='icon:close']").click()
-  modal().should("not.exist")
+describe("Documentation", () => {
+  it("shows clickable links in documentation", () => {
+    cy.visit("/fr/developers/api/catalog-management")
+    cy.get("h2#skus a").click()
+    cy.url().should("include", "#skus")
+  })
 })
 
-it("renders apps on the Apps page", () => {
-  cy.visit("/fr")
-  cy.contains("li", "Apps").click()
-  cy.contains("LivePepper")
+describe("Contact popup", () => {
+  it("opens the 'Contact us' modal and closes it", () => {
+    const modal = () => cy.get("[role='modal']")
+
+    cy.visit("/developers")
+    cy.contains("Contact us").click()
+    modal().should("be.visible")
+
+    modal().find("[data-testid='icon:close']").click()
+    modal().should("not.exist")
+  })
 })
 
-it("shows clickable links in documentation", () => {
-  cy.visit("/fr/developers/api/catalog-management")
-  cy.get("h2#skus a").click()
-  cy.url().should("include", "#skus")
+describe("Navigation", () => {
+  it("can navigate to the blog", () => {
+    cy.visit("/")
+    cy.contains("li", "Blog").click()
+    cy.url().should("include", "/blog")
+  })
 })
