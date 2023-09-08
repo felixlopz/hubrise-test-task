ab179fc0517737e8c8440de731fd2f4b4cf7b629
antmonnier <monniera@gmail.com>
2023-09-08 18:35:00 +0200

    Deploy: increase memory + add log in api/image

--- src/app/api/image/[...path]/route.ts
+++ src/app/api/image/[...path]/route.ts
@@ -5,11 +5,13 @@ import { contentDirectory } from "@utils/files"
 
 type Params = { path: Array<string> }
 
-// GET /api/image/[..path] will return the image at /content/[..path]
+// GET /api/image/[..path] returns the image at /content/[..path]
 export async function GET(request: Request, { params }: { params: Params }): Promise<Response> {
   const { path } = params
 
   const filePath = join(contentDirectory, ...path)
+  console.log(`Render image: ${filePath}`)
+
   const file = await fs.readFile(filePath)
 
   return new Response(file, {
