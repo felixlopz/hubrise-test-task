1175f03579ac6d192aad50b73f0e8bee02a48dd7
antmonnier <monniera@gmail.com>
2023-09-14 08:11:17 +0200

    Images in documentation: inline -> block

--- src/components/DocumentationMdxRenderer/components/Img/styles.ts
+++ src/components/DocumentationMdxRenderer/components/Img/styles.ts
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import { boxShadows, colors, mixin } from "@utils/styles"
 
 export const StyledImage = styled(Image)`
+  display: block;
   ${mixin.clickable};
   border: thin solid ${colors.borderLightest};
   box-shadow: ${boxShadows.image};
