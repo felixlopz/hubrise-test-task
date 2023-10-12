0c7c6bed7a1547e63bdd8e2e97168dcac4c25b0b
Felix Lopez <felixlopzd@gmail.com>
2023-10-12 13:00:29 -0400

    mobile nav improvements

--- src/layouts/Apps/Nav/Styles.ts
+++ src/layouts/Apps/Nav/Styles.ts
@@ -1,7 +1,8 @@
 import Link from "next/link"
 import styled, { css } from "styled-components"
 
-import { colors, mixin, sizes, zIndexValues } from "@utils/styles"
+import { breakpoints, colors, iconSizes, mixin, sizes, zIndexValues } from "@utils/styles"
+import Icon from "@components/Icon"
 
 export const StyledNav = styled.div<{ $isSticky: boolean }>`
   position: sticky;
@@ -14,9 +15,17 @@ export const StyledNav = styled.div<{ $isSticky: boolean }>`
     css`
       background-color: ${colors.primary};
       color: ${colors.white};
+      visibility: hidden;
+      opacity: 0;
+      pointer-events: none;
+
+      @media (min-width: ${breakpoints.large}) {
+        visibility: visible;
+        opacity: 1;
+        pointer-events: all;
+      }
     `}
 `
-
 export const List = styled.ul`
   ${mixin.container};
   display: flex;
@@ -37,4 +46,51 @@ export const StyledLink = styled(Link)<{ $isActive: boolean; $isSticky: boolean 
     color: ${({ $isSticky }) => ($isSticky ? colors.white : colors.primary)};
     text-decoration: underline;
   }
+`
+
+export const MobileNav = styled.div<{ $isSticky: boolean }>`
+  display: none;
+
+  ${(props) =>
+    props.$isSticky &&
+    css`
+      display: block;
+
+      @media (min-width: ${breakpoints.large}) {
+        display: none;
+      }
+    `}
+
+  position: sticky;
+  top: ${sizes.headerHeight};
+  z-index: ${zIndexValues.header};
+  padding: 0.5rem 0;
+  background-color: ${colors.primary};
+  color: ${colors.white};
+`
+
+export const MobileList = styled.ul<{ $isExpanded: boolean }>`
+  display: none;
+
+  ${(props) =>
+    props.$isExpanded &&
+    css`
+      display: block;
+    `}
+`
+
+export const MobileItem = styled.li`
+  padding: 0.25rem 1rem;
+`
+
+export const Title = styled.h5`
+  padding: 0.5rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const ArrowIcon = styled(Icon).attrs({ size: iconSizes._32 })`
+  color: ${colors.white};
+  cursor: pointer;
 `
